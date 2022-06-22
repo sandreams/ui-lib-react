cb6dee47e41d273a0a11aa7e9f4e90ce1fab2483
sanDream <fs517010856@gmail.com>
2022-06-23 07:45:08 +0800

    安装 dependency

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,9 @@
-console.log('darklin')
+import React from 'react'
+import ReactDOM from 'react-dom'
+type Prop = {
+  name: string
+}
+const App: React.FC<Prop> = (props) => {
+  return <div>{props.name}</div>
+}
+export default App
