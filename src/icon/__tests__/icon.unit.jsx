1aa3cc99bc7ff96269cf1e42b61afde6fc6a4eaf
wan <fs517010856@gmail.com>
2022-07-19 16:47:11 +0800

    add 配置React Testing Library并测试Icon组件点击

--- src/icon/__tests__/icon.unit.jsx
+++ src/icon/__tests__/icon.unit.jsx
@@ -1,9 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Icon from '../index';
+import { render, fireEvent, screen } from '@testing-library/react';
 describe('icon components', () => {
   it('test snapshots', () => {
     const json = renderer.create(<Icon name="editor" />).toJSON();
     expect(json).toMatchSnapshot();
+  });
+  it('test icon click', () => {
+    let n = 1;
+    const fn = () => {
+      n += 1;
+    };
+    const { queryByTestId, debug } = render(<Icon data-testid="aaa" name="editor" onClick={fn} />);
+    // debug();
+    fireEvent.click(queryByTestId('aaa'));
+    expect(n).toEqual(2);
   });
 });
